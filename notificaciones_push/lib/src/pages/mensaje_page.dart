d05beaaa0b8b2b9bcdff9fd1feb12afb5c197d18
Nathaliaaw <natalia4cardozo@gmail.com>
2021-06-24 20:09:42 -0500

    Genera la funcion de generaciond elos cuadros de texto en cuadros

--- notificaciones_push/lib/src/pages/mensaje_page.dart
+++ notificaciones_push/lib/src/pages/mensaje_page.dart
@@ -61,15 +61,47 @@ class _MensajePageState extends State<MensajePage> {
       padding: const EdgeInsets.all(3.0),
       decoration: BoxDecoration(
           border: Border.all(color: Colors.blueAccent),
-          borderRadius: new BorderRadius.only(
-            topLeft: const Radius.circular(40.0),
-            topRight: const Radius.circular(40.0),
-          )),
+          borderRadius: new BorderRadius.circular(40)),
+      child: ListView(
+        children:[  Column(
+          children: [
+            _mensajesEntrantes(Colors.blueAccent,
+                'colorC colorCcol orCcolorC se debe borrar la caja de texto al momento del envío de la notificación y se debe cerrar el teclado colorCc olorCcolor color CcolorC colorCc olorCc olorC C'),
+            ],
+        ),
+      ]
+       
+      ),
+    );
+  }
+
+  _mensajesEntrantes(Color colorC, String texto) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(top: 10,left: 5,right: 5),
+      constraints: BoxConstraints(
+        maxHeight: double.infinity,
+      ),
+      decoration: BoxDecoration(
+          color: colorC,
+          // border: Border.all(color: Colors.blueAccent),
+          borderRadius: new BorderRadius.circular(20)),
       child: Column(
         children: [
-          Text('My Awesome Border'),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              '$texto',
+              softWrap: true,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
+
 }
